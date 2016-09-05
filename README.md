6c9521b26e7175a68255043ab43d43b4eae80aaa
Dave Timmins <davetimmins@outlook.com>
2016-09-05 14:15:51 +1200

    tidy it

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # arcgis-fullscreen-widget
 An ArcGIS JS V4 widget that uses the Fullscreen API.
+
+Create it, then add it to your map UI
+
+```js
+require([
+  "custom-widgets/FullScreen"
+], function(FullScreen){
+
+  // create your map and view
+
+  var toggle = new FullScreen({
+    view: view
+  });
+  view.ui.add(toggle, "top-right");
+```
--- dojoConfig.js
+++ dojoConfig.js
@@ -1,12 +1,10 @@
 
-var origin = window.location.origin;
-var appName = window.location.pathname;
-var locationPath = origin.replace(/\/+$/, '') + appName;
+var locationPath = window.location.origin.replace(/\/+$/, '') + window.location.pathname;
 
 window.dojoConfig = {
   async: true,
   packages: [{
-      name: 'fesri',
+      name: 'custom-widgets',
       location: locationPath + 'src',
       main: 'FullScreen'
   }]
