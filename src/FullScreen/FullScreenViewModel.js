35577068c166c23936257b4a7e609388ea90ea9d
Dave Timmins <davetimmins@outlook.com>
2016-09-05 13:24:49 +1200

    use screenfull

--- src/FullScreen/FullScreenViewModel.js
+++ src/FullScreen/FullScreenViewModel.js
@@ -1,18 +1,21 @@
 define([
   "esri/core/Accessor", 
   "esri/core/HandleRegistry",
-  "dojo/dom-class"
-], function(Accessor, HandleRegistry, domClass) {
+  "dojo/dom-class",
+  "./screenfull",
+], function(Accessor, HandleRegistry, domClass, screenfull) {
+
   var state = {
-      disabled: "disabled",
-      ready: "ready"
-    },
-    mode = {
-      on: "on",
-      off: "off"
-    },
-    fullscreenEvent = 'fullscreenchange';
+    disabled: "disabled",
+    ready: "ready"
+  };
+  var mode = {
+    on: "on",
+    off: "off"
+  };
+
   return Accessor.createSubclass({
+
     declaredClass: "fesri.widgets.FullScreenViewModel",
     properties: {
       navigationMode: {},
@@ -22,44 +25,33 @@ define([
       },
       view: {}
     },
+
     constructor: function() {
 
       this._handles = new HandleRegistry;
-      this.toggle = this.toggle.bind(this);      
+      this.toggle = this.toggle.bind(this);   
+      this._fullScreenHandler = this._fullScreenHandler.bind(this);   
     },
+
     initialize: function() {
 
-      if ("onmozfullscreenchange" in document) {
-        this.fullscreenEvent = "mozfullscreenchange";
-      }
-      else if ("onwebkitfullscreenchange" in document) {
-        this.fullscreenEvent = "webkitfullscreenchange";
-      }
-      else if ("onmsfullscreenchange" in document) {
-        this.fullscreenEvent = "MSFullscreenChange";
-      }
-
-      document.addEventListener(this.fullscreenEvent, this._fullScreenHandler.bind(this));
+      if (screenfull.enabled) {
+        document.addEventListener(screenfull.raw.fullscreenchange, this._fullScreenHandler);
+      }      
     },
+
     destroy: function() {
+      
+      if (screenfull.enabled) {
+        document.removeEventListener(screenfull.raw.fullscreenchange, this._fullScreenHandler);
+      } 
 
       this._handles.destroy();
-      document.removeEventListener(this.fullscreenEvent, this._fullScreenHandler);
     },
-    _handles: null,
-    state: state.disabled,
-    _stateGetter: function() {
 
-      return this.get("view.ready") ? state.ready : state.disabled
-    },
     _fullScreenHandler: function () {
 
-        this.mode = 
-          document.fullscreenElement ||
-          document.msFullscreenElement ||
-          document.webkitFullscreenElement ||
-          document.mozFullScreenElement          
-          ? mode.on : mode.off;
+        this.mode = screenfull.isFullscreen ? mode.on : mode.off;
 
         if (this.mode === mode.off) {
           domClass.remove(this.view.container, 'view-fullscreen-on');
@@ -68,12 +60,23 @@ define([
           domClass.add(this.view.container, 'view-fullscreen-on');
         }
     },
+
+    _handles: null,
+
+    state: state.disabled,
+    _stateGetter: function() {
+
+      return this.get("view.ready") ? state.ready : state.disabled
+    },
+    
     mode: mode.off,
     _modeGetter: function() {
 
       return this._get("mode") || mode.off;
     },
+
     view: null,
+    
     toggle: function(e) {
 
       e.preventDefault();
@@ -83,43 +86,19 @@ define([
       }
 
       this.mode = this.mode !== mode.on ? mode.on : mode.off;
+
+      if (!screenfull.enabled) {
+        return;
+      }
              
       if (this.mode === mode.off) {
                 
-        this._exitFullscreen();
+        screenfull.exit();
       }
       else {
 
-        this._requestFullscreen(this.view.container);
+        screenfull.request(this.view.container);
       }      
-    },
-    _requestFullscreen: function(element) {
-
-      if (element.requestFullscreen) {
-        element.requestFullscreen();
-      } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-      } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-      } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-      } else {
-        console.log('Fullscreen API is not supported.');
-      }
-    },
-    _exitFullscreen: function() {
-
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-      } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-      } else {
-        console.log('Fullscreen API is not supported.');
-      }
-    }
+    }    
   })
 });
